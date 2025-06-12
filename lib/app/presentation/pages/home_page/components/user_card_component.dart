12ea8ccf5c61a2abfe9680e252e148240278fd99
Brenne Santos <68624464+Brennez@users.noreply.github.com>
2025-06-12 07:37:40 -0300

    feat: creating homepage and componentizing wigdets

---
+++ lib/app/presentation/pages/home_page/components/user_card_component.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:git_app/app/data/models/models_export.dart';
+
+class UserCardComponent extends StatefulWidget {
+  final UserModel user;
+  const UserCardComponent({
+    super.key,
+    required this.user,
+  });
+
+  @override
+  State<UserCardComponent> createState() => _UserCardComponentState();
+}
+
+class _UserCardComponentState extends State<UserCardComponent> {
+  bool _isExpanded = false;
+
+  void _toggleExpansion() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8.0),
+      child: ExpansionPanelList(
+        elevation: 1,
+        animationDuration: const Duration(milliseconds: 300),
+        expansionCallback: (int index, bool isExpanded) {
+          _toggleExpansion();
+        },
+        children: [
+          ExpansionPanel(
+            backgroundColor: Colors.grey.shade100,
+            headerBuilder: (context, isExpanded) {
+              return ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage(widget.user.profilePictureUrl ??
+                      'https://avatars.githubusercontent.com/u/9919?v=4'),
+                ),
+                title: Text(widget.user.name),
+                subtitle: Text(widget.user.bio ?? 'Sem biografia'),
+                onTap: () => _toggleExpansion(),
+              );
+            },
+            body: Column(
+              children: [
+                ListTile(
+                  title: const Text('Username'),
+                  subtitle: Text(widget.user.username),
+                  leading: Icon(Icons.alternate_email_rounded,
+                      color: Colors.deepPurple),
+                ),
+                ListTile(
+                  title: const Text('Localização'),
+                  subtitle: Text(widget.user.location ?? 'Não informado'),
+                  leading: Icon(Icons.location_on, color: Colors.red),
+                ),
+                ListTile(
+                  title: const Text('Repositórios'),
+                  subtitle: Text(widget.user.repositories.toString()),
+                  leading: Icon(Icons.folder, color: Colors.blue),
+                ),
+                ListTile(
+                  title: const Text('Seguidores'),
+                  subtitle: Text(widget.user.followers.toString()),
+                  leading: Icon(Icons.people, color: Colors.green),
+                ),
+                ListTile(
+                  title: const Text('Seguindo'),
+                  subtitle: Text(widget.user.following.toString()),
+                  leading: Icon(Icons.person_add, color: Colors.orange),
+                ),
+              ],
+            ),
+            isExpanded: _isExpanded,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/app/presentation/pages/home_page/home_page.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:git_app/app/data/models/models_export.dart';
+import 'package:git_app/app/presentation/controllers/controllers_export.dart';
+import 'package:git_app/core/dependecies/dependency_injection.dart';
+
+import '../../enums/screen_state_enum.dart';
+import 'components/components_export.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final UserInfoController _userInfoController = injector<UserInfoController>();
+
+  final CacheController _cacheController = injector<CacheController>();
+
+  final TextEditingController _usernameTextController = TextEditingController();
+
+  Future<void> _handleSearch() async {
+    final UserModel? cachedUser =
+        await _cacheController.getUserCache(_usernameTextController.text);
+
+    if (cachedUser != null) {
+      _userInfoController.setUserInfo(cachedUser);
+    } else {
+      await _userInfoController.getUserInfo(_usernameTextController.text);
+
+      if (_userInfoController.userInfo != null) {
+        await _cacheController.saveUserCache(_usernameTextController.text,
+            _userInfoController.userInfo!.toMap());
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Git App'),
+        centerTitle: true,
+      ),
+      endDrawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color(0xFF57606A),
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home, color: Color(0xFF24292F)),
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.history,
+                color: Color(0xFF24292F),
+              ),
+              title: Text('Histórico de buscas'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.clear_all,
+                color: Color(0xFF24292F),
+              ),
+              title: Text('Limpar cache'),
+              onTap: () async {
+                await _handleClearCache();
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: SizedBox(
+                    height: 40,
+                    child: TextField(
+                      controller: _usernameTextController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide(color: Colors.grey.shade400),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        isDense: true,
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 10),
+                        suffixIcon: Icon(Icons.search,
+                            color: Colors.grey.shade600, size: 20),
+                        hintText: 'Digite um username',
+                        hintStyle: TextStyle(
+                          color: Colors.grey.shade400,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF57606A),
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                  onPressed: () {
+                    _handleSearch();
+                    FocusScope.of(context).unfocus();
+                  },
+                  child: const Text(
+                    'Buscar',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Obx(
+              () {
+                if (_userInfoController.screenState == ScreenState.loading) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (_userInfoController.userInfo != null) {
+                  return UserCardComponent(
+                    user: _userInfoController.userInfo!,
+                  );
+                } else if (_userInfoController.screenState ==
+                    ScreenState.error) {
+                  return Center(
+                    child: Text(
+                      "Erro ao buscar usuário",
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  );
+                }
+                return const Center(
+                  child: Text(
+                    "Digite um username para buscar",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _handleClearCache() async {
+    await _cacheController.clearAllCache();
+    _userInfoController.setUserInfo(null);
+    _usernameTextController.clear();
+  }
+}
